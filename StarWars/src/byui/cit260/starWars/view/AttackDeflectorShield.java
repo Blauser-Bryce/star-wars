9a53a9ca14b0edffdc85987a8b409ebed082eb49
Edvaldo de Oliveira Melo <familiamelo@gmail.com>
2016-11-03 08:17:59 -0200

    Updated Attack Deflector Shield

--- StarWars/src/byui/cit260/starWars/view/AttackDeflectorShield.java
+++ StarWars/src/byui/cit260/starWars/view/AttackDeflectorShield.java
@@ -80,11 +80,34 @@ public class AttackDeflectorShield {
         return false;
     }
 
-    private void damageDeflectorShield() {
-        System.err.println("*** Damage Tie Fighter. ***");
+    private int damageDeflectorShield() {
+        
+        int location = 2;//test turbo Laser location
+        int target = 2;// test target location
+        int health = 100;// test health
+        int missile = 100;// test 
+        int damage = 30;// test damage done
+        double amplifier = 1.5; //test amplifier
+        double amount = 30;// test amount
+        
+        while (missile > 0)
+            missile --;
+        if (location == target && health >= 1) {
+            health =- damage;
+            System.out.println("*** Damage Deflector Shield. ***" + health + "health remaining");
+            return health;
+        }
+        
+        else if (location == target && health <= 0) {
+            System.out.println("Deflector Shield Destroyed.");
+        }
+        
+        else
+            System.out.println("Miss");
+            return health;
     }
 
     private void evasiveManeuver() {
-        System.err.println("*** Evasive Maneuver Called. ***");
+        System.out.println("*** Evasive Maneuver Called. ***");
     }
 }
