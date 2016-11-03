75b99192c025ad0db2d61155c303a8ffb509c94e
Edvaldo de Oliveira Melo <familiamelo@gmail.com>
2016-11-03 08:19:16 -0200

    Updated Attack Tie Fighter

--- StarWars/src/byui/cit260/starWars/view/AttackTieFighter.java
+++ StarWars/src/byui/cit260/starWars/view/AttackTieFighter.java
@@ -102,11 +102,34 @@ public class AttackTieFighter {
         return false;
     }
 
-    private void damageTieFighter() {
-        System.err.println("*** Damage Tie Fighter. ***");
+    private int damageTieFighter() {
+        
+        int location = 2;//test turbo Laser location
+        int target = 2;// test target location
+        int health = 100;// test health
+        int missile = 100;// test 
+        int damage = 30;// test damage done
+        double amplifier = 1.5; //test amplifier
+        double amount = 30;// test amount
+        
+        while (missile > 0)
+            missile --;
+        if (location == target && health >= 1) {
+            health =- damage;
+            System.out.println("*** Damage Tie Fighter. ***" + health + "health remaining");
+            return health;
+        }
+        
+        else if (location == target && health <= 0) {
+            System.out.println("Tie Fighter Destroyed.");
+        }
+        
+        else
+            System.out.println("Miss");
+            return health;
     }
 
     private void evasiveManeuver() {
-        System.err.println("*** Evasive Maneuver Called. ***");
+        System.out.println("*** Evasive Maneuver Called. ***");
     }
 }
