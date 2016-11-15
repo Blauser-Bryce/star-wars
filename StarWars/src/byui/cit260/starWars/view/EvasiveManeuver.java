6be189529cb47c402ad81cdf45f7c23df00b685f
Edvaldo de Oliveira Melo <familiamelo@gmail.com>
2016-11-15 00:10:37 -0200

    Updated Evasive Maneuver

--- StarWars/src/byui/cit260/starWars/view/EvasiveManeuver.java
+++ StarWars/src/byui/cit260/starWars/view/EvasiveManeuver.java
@@ -7,7 +7,8 @@ package byui.cit260.starWars.view;
 
 /**
  *
- * @author Edvaldo Melo
+ *@author Edvaldo Melo
+ **/
  
 public class EvasiveManeuver extends View {
     
@@ -17,19 +18,56 @@ public class EvasiveManeuver extends View {
     public EvasiveManeuver() {
         super ("\n"
                 + "\n--------------------------------"
+                + "\nYou Are Under Attack!           "
+                + "\nYou Are An Easy Target, Now!    "
+                + "\nYou Are On Target!!!            "
+                + "\nPrepare to Evasive Maneuver     "
                 + "\n| Game Menu                    |"
                 + "\n--------------------------------"
-                + "\nU - Evade Up"
-                + "\nD - Evade Down"
-                + "\nR - Evade Right"
-                + "\nL - Evade Left"
-                + "\nX - Exit Evade Menu"
+                + "\nU - Evade Up                    "
+                + "\nD - Evade Down                  "
+                + "\nR - Evade Right                 "
+                + "\nL - Evade Left                  "
+                + "\nX - Exit Evade Menu             "
                 + "\n--------------------------------");
     }
 
     @Override
     public boolean doAction(String value) {
         value = value.toUpperCase(); // convert to upper
+        
+        switch (value) {
+            case "U": // Prompt for Evade Up
+                this.evadeUp();
+                break;
+            case "D": // Prompt for Evade Down
+                this.evadeDown();
+                break;
+            case "R": // Prompt for Evade Right
+                this.evadeRight();
+                break;
+            case "L": // Prompt for Evade Left
+                this.evadeLeft();
+                break;
+            default:
+                System.err.println("\n*** Invalid selection. *** Try again.");
+        }
+        return false;
+    }
+
+    private void evadeUp() {
+        
+    }
+
+    private void evadeDown() {
+    
+    }
+
+    private void evadeRight() {
+    
+    }
+
+    private void evadeLeft() {
+    
     }
 }
-*/
