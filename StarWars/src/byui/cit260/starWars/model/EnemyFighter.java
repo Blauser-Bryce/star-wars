32b1712c04773a0061bfd33cfd03444bece979ef
Edvaldo O. Melo <Edvaldo O. Melo@192.168.25.17>
2016-12-16 00:11:26 -0200

    Updated EnemyFighter

--- StarWars/src/byui/cit260/starWars/model/EnemyFighter.java
+++ StarWars/src/byui/cit260/starWars/model/EnemyFighter.java
@@ -6,16 +6,69 @@
 package byui.cit260.starWars.model;
 
 import java.io.Serializable;
+import java.util.Objects;
 
 
 /**
  *
  * @author Bryce Blauser
  */
-public class EnemyFighter extends Target implements Serializable{
+public class EnemyFighter extends Target implements Serializable {
+    
+    private int noOfFighter;
+    private String enemyFigthersLocation;
     
     public EnemyFighter() {
 
     }
 
+    public int getNoOfFighter() {
+        return noOfFighter;
+    }
+
+    public String getEnemyFigthersLocation() {
+        return enemyFigthersLocation;
+    }
+
+    public void setNoOfFighter(int noOfFighter) {
+        this.noOfFighter = noOfFighter;
+    }
+
+    public void setEnemyFigthersLocation(String enemyFigthersLocation) {
+        this.enemyFigthersLocation = enemyFigthersLocation;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 7;
+        hash = 19 * hash + this.noOfFighter;
+        hash = 19 * hash + Objects.hashCode(this.enemyFigthersLocation);
+        return hash;
+    }
+    
+    @Override
+    public String toString() {
+        return "EnemyFighter{" + "noOfFighter=" + noOfFighter + ", enemyFigthersLocation=" + enemyFigthersLocation + '}';
+    }
+    
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final EnemyFighter other = (EnemyFighter) obj;
+        if (this.noOfFighter != other.noOfFighter) {
+            return false;
+        }
+        if (!Objects.equals(this.enemyFigthersLocation, other.enemyFigthersLocation)) {
+            return false;
+        }
+        return true;
+    }
 }
