7c139e372fec447b90ee61af8b12c49944277bbd
Edvaldo O. Melo <Edvaldo O. Melo@192.168.25.17>
2016-12-17 12:46:32 -0200

    Updated TargetControl

--- StarWars/src/byui/cit260/starWars/control/TargetControl.java
+++ StarWars/src/byui/cit260/starWars/control/TargetControl.java
@@ -9,6 +9,7 @@ import byui.cit260.starWars.model.EnemyFighter;
 import byui.cit260.starWars.model.Target;
 import byui.cit260.starWars.model.Target.targetType;
 import exceptions.TargetControlException;
+import java.util.Scanner;
 import starwars.StarWars;
 
 /**
@@ -16,26 +17,26 @@ import starwars.StarWars;
  * @author Bryce Blauser
  */
 public class TargetControl {
-    
-    public String applyDamage(Target target, double amplifier, double amount)  {
-        
+
+    public String applyDamage(Target target, double amplifier, double amount) {
+
         double damageShield;
         double damageHealth;
         double applyShield;
         double applyHealth;
-        
+
         if (target == null) {
             return "Error: Invalid Target";
         }
-        
-        if (amount <= 0 ) {
+
+        if (amount <= 0) {
             return "Error: Invalid Damage value";
         }
-        
+
         if (amplifier < 1 || amplifier > 2) {
             return "Error: Invalid Amplifier value";
         }
-        
+
         if (target.getTargetShield() > 50) {
             damageShield = amount * 0.75 * amplifier;
             damageHealth = amount * 0.5 * amplifier;
@@ -46,42 +47,42 @@ public class TargetControl {
             damageShield = 0;
             damageHealth = amount * amplifier;
         }
-        
+
         applyShield = target.getTargetShield() - damageShield;
         applyHealth = target.getTargetHealth() - damageHealth;
-        
+
         target.setTargetShield(applyShield);
         target.setTargetHealth(applyHealth);
-        
+
         // Check if exhaust port is destroyed
         if (target.getTargetHealth() <= 0 && target.getTargetType() == targetType.exhaustPort) {
             return "Death Star Destroyed!!!!\nGame Over!\nCongratulations!  You saved the galaxy";
         }
-        
+
         if (target.getTargetHealth() <= 0) {
             return "Target Destroyed";
         }
-        
+
         return "Target damage done: Shield " + damageShield + " Health " + damageHealth + ", Remaining: Shield " + applyShield + " Health " + applyHealth;
     }
-    
+
     public String applyRepair(Target target, double amplifier, double amount) {
-     
+
         double repairHealth;
         double repairShield;
         double applyRepairShield;
         double applyRepairHealth;
-        
+
         if (target == null) {
             return "Error: Invalid Target";
         }
-        if (amount <= 0 ) {
+        if (amount <= 0) {
             return "Error: Invalid Amount value";
         }
         if (amplifier < 1 || amplifier > 2) {
             return "Error: Invalid Amplifier value";
-        } else if (target.getTargetHealth() > 100 && target.getTargetShield() < 100){
-            repairHealth = 100; 
+        } else if (target.getTargetHealth() > 100 && target.getTargetShield() < 100) {
+            repairHealth = 100;
             repairShield = amount * 0.75 * amplifier;
         } else if (target.getTargetHealth() > 50) {
             repairShield = amount * 0.75 * amplifier;
@@ -92,37 +93,37 @@ public class TargetControl {
         } else {
             return "Player at full health";
         }
-        
+
         applyRepairShield = repairShield + target.getTargetShield();
         applyRepairHealth = repairHealth + target.getTargetHealth();
-        
+
         target.setTargetShield(applyRepairShield);
         target.setTargetHealth(applyRepairHealth);
-        
+
         if (target.getTargetHealth() > 100) {
             applyRepairHealth = 100;
         }
-         if (target.getTargetShield() > 100) {
+        if (target.getTargetShield() > 100) {
             applyRepairShield = 100;
-         }
-        if (target.getTargetHealth() >= 100 && target.getTargetShield() >= 100) {
-            
-           return "Target at Full Health";
         }
-            
+        if (target.getTargetHealth() >= 100 && target.getTargetShield() >= 100) {
+
+            return "Target at Full Health";
+        }
+
         return "Target Repair done: Shield " + repairShield + " Health " + repairHealth + ", Remaining: Shield " + applyRepairShield + " Health " + applyRepairHealth;
-    }    
-    
+    }
+
     public int getMaxEnemyHealth(EnemyFighter[] enemyFighterList) throws TargetControlException {
-        
+
         double maxValue = enemyFighterList[0].getTargetHealth();
         int index = 0;
         int enemyIndex = 0;
-        
+
         if (enemyFighterList == null || enemyFighterList.length == 0) {
-             throw new TargetControlException("No enemies found in current list");
+            throw new TargetControlException("No enemies found in current list");
         }
-        
+
         for (EnemyFighter enemyFighter : enemyFighterList) {
             if (enemyFighter.getTargetHealth() > maxValue) {
                 maxValue = enemyFighter.getTargetHealth();
@@ -130,56 +131,73 @@ public class TargetControl {
             }
             index++;
         }
-        
-        return enemyIndex;   
+
+        return enemyIndex;
     }
-    
+
     public int getDeflectorShieldCount() {
-        
+
         EnemyFighter[] enemyFighterList = StarWars.getCurrentGame().getEnemyFighters();
-        
+
         int deflectorShieldsRemaining = 0;
-        
+
         for (EnemyFighter enemyFighter : enemyFighterList) {
-            if (enemyFighter.getTargetHealth() > 0 && enemyFighter.getTargetType() ==  EnemyFighter.targetType.deflectorShield) {
-               deflectorShieldsRemaining++;
-            } 
+            if (enemyFighter.getTargetHealth() > 0 && enemyFighter.getTargetType() == EnemyFighter.targetType.deflectorShield) {
+                deflectorShieldsRemaining++;
+            }
         }
         return deflectorShieldsRemaining;
     }
-    
+
     public int getLowEnemyHealth(EnemyFighter[] enemyFighterList) {
         double lowValue = enemyFighterList[0].getTargetHealth();
         int index = 0;
         int enemyIndex = -1;
-        
+
         for (EnemyFighter enemyFighter : enemyFighterList) {
             if (enemyFighter.getTargetHealth() <= lowValue) {
-               lowValue = enemyFighter.getTargetHealth();
+                lowValue = enemyFighter.getTargetHealth();
                 enemyIndex = index;
-            } 
-            
+            }
+
             index++;
         }
-        
-        return enemyIndex;   
+
+        return enemyIndex;
     }
-    
+
     public int getLengthEnemyHealth(EnemyFighter[] enemyFighterList) {
         int size = enemyFighterList.length;
         return size;
     }
-    
+
     public double getAvgEnemyHealth(EnemyFighter[] enemyFighterList) {
-       
+
         double total = 0;
-        for (int i = 0; i < enemyFighterList.length -1 ; i++) {
+        for (int i = 0; i < enemyFighterList.length - 1; i++) {
             total = total + enemyFighterList[i].getTargetHealth();
+        }
+
+        double average = total / (enemyFighterList.length - 1);
+
+        return average;
+
     }
+    
+    // Added by Edvaldo Melo
+    public double getTotalPriceOfItem() {
         
-    double average = total / (enemyFighterList.length-1);         
+        Scanner inFile = new Scanner(System.in);
         
-        return average;            
-       
-    }  
+        // get the price of the item entered by the end user
+        String priceAsString = inFile.next();
+        
+        // convert price ftom String to a fouble value
+        double priceAsDouble = Double.parseDouble(priceAsString);
+        
+        // add tax to the price
+        double total = priceAsDouble + priceAsDouble * 0.7;
+        
+        return total;
+    }
 }
