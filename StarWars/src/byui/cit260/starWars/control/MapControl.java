0b6073cb00969651c17f446c15e5dc9f3236daa0
Edvaldo O. Melo <Edvaldo O. Melo@192.168.25.17>
2016-12-17 16:54:54 -0200

    Updated MapControl in control package

--- StarWars/src/byui/cit260/starWars/control/MapControl.java
+++ StarWars/src/byui/cit260/starWars/control/MapControl.java
@@ -5,10 +5,13 @@
  */
 package byui.cit260.starWars.control;
 
+import byui.cit260.starWars.model.EnemyFighter;
 import byui.cit260.starWars.model.Location;
 import byui.cit260.starWars.model.Location.locationStatus;
 import byui.cit260.starWars.model.Map;
 import byui.cit260.starWars.view.SupplyShipView;
+import exceptions.MapControlException;
+import java.awt.Point;
 import java.io.PrintWriter;
 import starwars.StarWars;
 
@@ -17,23 +20,23 @@ import starwars.StarWars;
  * @author Bryce Blauser
  */
 public class MapControl {
-    
+
     private static final PrintWriter console = StarWars.getOutFile();
-    
+
     public static Map createMap() {
         // Create the map
         Map map = new Map();
         map = map.createMap();
-                
+
         return map;
     }
-        
+
     public boolean goToLocation(Location location) {
-        
+
         if (location.getStatus() == locationStatus.locked) {
             return false;
         }
-        
+
         // Check if the player moved to a new scene and perform action if necessary
         String displaySymbol = location.getScene().getDisplaySymbol();
 
@@ -51,33 +54,49 @@ public class MapControl {
                 break;
 
             default:
-                //console.println("\n*** You are at an unknown location ***");
+            //console.println("\n*** You are at an unknown location ***");
         }
-        
+
         return true;
     }
-    
+
     public void unlockTrench() {
-        
+
         Map map = StarWars.getCurrentGame().getMap();
         Location[][] locations = map.getLocations();
-        
+
         int noOfRows = map.getNoOfRows();
         int noOfCols = map.getNoOfColumns();
-        
+
         for (int row = 0; row < noOfRows; row++) {
             for (int col = 0; col < noOfCols; col++) {
                 locations[row][col].setStatus(Location.locationStatus.unlocked);
             }
         }
     }
-    
+
     private void moveToSupplyShip() {
         SupplyShipView supplyView = new SupplyShipView();
         supplyView.display();
     }
-    
+
     static void moveActorsToStartingLocation(Map map) {
         console.println("\n *** moveActorsToStartingLocation called ***");
     }
+
+    public static void moveEnemyFighterToLocation(EnemyFighter enemyFighter, Point coordinates) throws MapControlException {
+
+        Map map = StarWars.getCurrentGame().getMap();
+        int newRow = coordinates.x - 1;
+        int newColumn = coordinates.x - 1;
+
+        if (newRow < 0 || newRow >= map.getNoOfRows() || newColumn < 0
+                || newColumn >= map.getNoOfColumns()) {
+            throw new MapControlException("Can not move enemy fighter to location "
+                    + coordinates.x + ", " + coordinates.y + " because that location "
+                    + " is outside the bounds of the map.");
+        }
+        
+    }
+
 }
