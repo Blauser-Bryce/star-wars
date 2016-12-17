bf41f651f70db4cbe9a36f04fd758d96c7d81e99
Edvaldo O. Melo <Edvaldo O. Melo@192.168.25.17>
2016-12-17 12:45:46 -0200

    Updated GameMenuView

--- StarWars/src/byui/cit260/starWars/view/GameMenuView.java
+++ StarWars/src/byui/cit260/starWars/view/GameMenuView.java
@@ -8,6 +8,7 @@ package byui.cit260.starWars.view;
 import byui.cit260.starWars.control.TargetControl;
 import byui.cit260.starWars.model.EnemyFighter;
 import byui.cit260.starWars.model.EvasiveManeuver;
+import byui.cit260.starWars.model.Item;
 import byui.cit260.starWars.model.Map;
 import exceptions.TargetControlException;
 import starwars.StarWars;
@@ -22,18 +23,18 @@ public class GameMenuView extends View {
 
     private String menu;
     MainMenuView mainMenu = new MainMenuView();
-    
+
     public GameMenuView() {
         super("\n"
                 + "\n--------------------------------"
                 + "\n| Game Menu                    |"
                 + "\n--------------------------------"
                 + "\nM - View Map"
-               // + "\nL - Attack Turbo Laser"
-               // + "\nF - Attack Tie Fighter"
-               // + "\nS - Attack Defletor Shield"
-               // + "\nE - Evasive Maneuver"
-               // + "\nT - Fire Torpedo"
+                // + "\nL - Attack Turbo Laser"
+                // + "\nF - Attack Tie Fighter"
+                // + "\nS - Attack Defletor Shield"
+                // + "\nE - Evasive Maneuver"
+                // + "\nT - Fire Torpedo"
                 + "\nA - View Ammunition"
                 + "\nV - View Strongest Enemy"
                 + "\nW - View Weakest Enemy"
@@ -42,14 +43,14 @@ public class GameMenuView extends View {
                 + "\nP - Print Remaining Enemies"
                 + "\nN - Select target"
                 + "\nX - Exit"
-                + "\n--------------------------------"); 
+                + "\n--------------------------------");
     }
 
     @Override
     public boolean doAction(String value) {
-        
+
         value = value.toUpperCase(); //convert to upper
-        
+
         switch (value) {
             case "M": // view map
                 this.viewMap();
@@ -90,22 +91,25 @@ public class GameMenuView extends View {
             case "N": // Select Target View
                 this.targetView();
                 break;
+            case "I": // Select Total
+                this.viewTotalPriceOfItem();
+                break;
             default:
                 console.println("\n*** Invalid selection *** Try again");
         }
-        
+
         return false;
     }
-  
+
     private void viewMap() {
-       
-       Map map = new Map();
-       map = StarWars.getCurrentGame().getMap();
-       map.displayMap(map);
-       
-       // display the map view
-       MapView mapView = new MapView();
-       mapView.display();
+
+        Map map = new Map();
+        map = StarWars.getCurrentGame().getMap();
+        map.displayMap(map);
+
+        // display the map view
+        MapView mapView = new MapView();
+        mapView.display();
     }
 
     private void viewAmmuntion() {
@@ -118,70 +122,71 @@ public class GameMenuView extends View {
         SelectTargetView targetView = new SelectTargetView();
         targetView.display();
     }
-    
+
     private void viewStrongestEnemy() {
-        
+
         TargetControl targetControl = new TargetControl();
         EnemyFighter[] enemyFighterList = StarWars.getCurrentGame().getEnemyFighters();
-        
+
         try {
             int strongestEnemy = targetControl.getMaxEnemyHealth(enemyFighterList);
             console.println("\n The Strongest Enemy is: "
-                              +"\n Name:\t" + enemyFighterList[strongestEnemy].getTargetName()
-                              +"\n Location: \t(" + enemyFighterList[strongestEnemy].getTargetLocation().getRow() + "," 
-                              + enemyFighterList[strongestEnemy].getTargetLocation().getColumn() + ")"
-                              + "\n Health: \t" + enemyFighterList[strongestEnemy].getTargetHealth());
+                    + "\n Name:\t" + enemyFighterList[strongestEnemy].getTargetName()
+                    + "\n Location: \t(" + enemyFighterList[strongestEnemy].getTargetLocation().getRow() + ","
+                    + enemyFighterList[strongestEnemy].getTargetLocation().getColumn() + ")"
+                    + "\n Health: \t" + enemyFighterList[strongestEnemy].getTargetHealth());
         } catch (TargetControlException me) {
             ErrorView.display(this.getClass().getName(), me.getMessage());
         }
     }
+
     /*gary moser*/
     private void viewWeakEnemy() {
         TargetControl targetControl = new TargetControl();
         EnemyFighter[] enemyFighterList = StarWars.getCurrentGame().getEnemyFighters();
-        
+
         int weakEnemy = targetControl.getLowEnemyHealth(enemyFighterList);
-                
+
         if (weakEnemy >= 0) {
             console.println("\n The weakest Enemy is: "
-                              +"\n Name:\t" + enemyFighterList[weakEnemy].getTargetName()
-                              +"\n Location: \t(" + enemyFighterList[weakEnemy].getTargetLocation().getRow() + "," 
-                              + enemyFighterList[weakEnemy].getTargetLocation().getColumn() + ")"
-                              + "\n Health: \t" + enemyFighterList[weakEnemy].getTargetHealth()      
+                    + "\n Name:\t" + enemyFighterList[weakEnemy].getTargetName()
+                    + "\n Location: \t(" + enemyFighterList[weakEnemy].getTargetLocation().getRow() + ","
+                    + enemyFighterList[weakEnemy].getTargetLocation().getColumn() + ")"
+                    + "\n Health: \t" + enemyFighterList[weakEnemy].getTargetHealth()
             );
         } else {
             console.println("\n No enemies exist");
         }
     }
-         /*gary moser*/
+
+    /*gary moser*/
     private void viewRemainingEnemy() {
         TargetControl targetControl = new TargetControl();
         EnemyFighter[] enemyFighterList = StarWars.getCurrentGame().getEnemyFighters();
         int remainingEnemy = targetControl.getLengthEnemyHealth(enemyFighterList);
-        
-        if (remainingEnemy > 0){
-        console.println("\n" + remainingEnemy + " Enemies Remaining");
-        }
-        else{
-        console.println("No Ememies Remain");
+
+        if (remainingEnemy > 0) {
+            console.println("\n" + remainingEnemy + " Enemies Remaining");
+        } else {
+            console.println("No Ememies Remain");
         }
     }
-    
+
     /*gary moser */
     private void viewAvgEnemy() {
         TargetControl targetControl = new TargetControl();
         EnemyFighter[] enemyFighterList = StarWars.getCurrentGame().getEnemyFighters();
         double avgEnemy = targetControl.getAvgEnemyHealth(enemyFighterList);
-        
+
         console.println("\n Average Enemy Health " + avgEnemy);
     }
-    
+
     /*gary Moser */
     private void remainingEnemiesp() {
-     
+
         console.println("\n\nEnter the file path for the file where the game is to be saved.");
         String filePath = this.getInput();
-        
+
         try {
             saveReport(filePath);
             console.println("\nFile successfully saved to: " + filePath);
@@ -189,21 +194,32 @@ public class GameMenuView extends View {
             ErrorView.display("GameMenuView", ex.getMessage());
         }
     }
-     public void saveReport(String filePath) throws GameControlException {
+
+    public void saveReport(String filePath) throws GameControlException {
         EnemyFighter[] enemyFighterList = StarWars.getCurrentGame().getEnemyFighters();
-        
+
         try (PrintWriter out = new PrintWriter(filePath)) {
             out.println("\n\n      Remaining Enemies Report");
-            out.printf("%n%-20s%8s","Target","R,C");
-            out.printf("%n%-20s%8s","---------------","--------");
-            
+            out.printf("%n%-20s%8s", "Target", "R,C");
+            out.printf("%n%-20s%8s", "---------------", "--------");
+
             // for each inventory item
             for (EnemyFighter enemyFighter : enemyFighterList) {
-                out.printf("%n%-20s%1d,%1d", enemyFighter.getTargetName(),enemyFighter.getTargetLocation().getRow(),enemyFighter.getTargetLocation().getColumn());
+                out.printf("%n%-20s%1d,%1d", enemyFighter.getTargetName(), enemyFighter.getTargetLocation().getRow(), enemyFighter.getTargetLocation().getColumn());
             }
         } catch (Exception e) {
             throw new GameControlException(e.getMessage());
         }
     }
-    
+
+    // By Edvaldo Melo
+    private void viewTotalPriceOfItem() {
+        TargetControl targetControl = new TargetControl();
+        Item[] ItemList;
+        ItemList = StarWars.getCurrentGame().getInventory();
+        double total = targetControl.getTotalPriceOfItem();
+
+        console.print("\n Total of Item " + total);
+    }
+
 }
