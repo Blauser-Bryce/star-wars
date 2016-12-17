6033daea399431bc363e29c0c4ed87f22f097b01
Edvaldo O. Melo <Edvaldo O. Melo@192.168.25.17>
2016-12-17 11:23:20 -0200

    Updated EnemyFighter

--- StarWars/src/byui/cit260/starWars/model/EnemyFighter.java
+++ StarWars/src/byui/cit260/starWars/model/EnemyFighter.java
@@ -5,19 +5,20 @@
  */
 package byui.cit260.starWars.model;
 
+import java.awt.Point;
 import java.io.Serializable;
 import java.util.Objects;
-
+import starwars.StarWars;
 
 /**
  *
  * @author Bryce Blauser
  */
-public class EnemyFighter extends Target implements Serializable{
-    
+public class EnemyFighter extends Target implements Serializable {
+
     private int noOfFighter;
     private String enemyFigthersLocation;
-    
+
     public EnemyFighter() {
 
     }
@@ -45,12 +46,12 @@ public class EnemyFighter extends Target implements Serializable{
         hash = 19 * hash + Objects.hashCode(this.enemyFigthersLocation);
         return hash;
     }
-    
+
     @Override
     public String toString() {
         return "EnemyFighter{" + "noOfFighter=" + noOfFighter + ", enemyFigthersLocation=" + enemyFigthersLocation + '}';
     }
-    
+
     @Override
     public boolean equals(Object obj) {
         if (this == obj) {
@@ -66,9 +67,45 @@ public class EnemyFighter extends Target implements Serializable{
         if (this.noOfFighter != other.noOfFighter) {
             return false;
         }
-        if (!Objects.equals(this.enemyFigthersLocation, other.enemyFigthersLocation)) {
-            return false;
+        return Objects.equals(this.enemyFigthersLocation, other.enemyFigthersLocation);
+    }
+
+    EnemyFighter[] enemyFighterList = StarWars.getCurrentGame().getEnemyFighters();
+
+    public enum EnemyFighters implements Serializable {
+        BlackLeader1("Black Leader #1"),
+        BlackLeader2("Black Leader #2"),
+        BlackLeader3("Black Leader #3"),
+        BlackLeader4("Black Leader #4"),
+        BlackLeader5("Black Leader #5"),
+        BlackLeader6("Black Leader #6"),
+        GrayLeader1("Gray Leader #1"),
+        GrayLeader2("Gray Leader #2"),
+        GrayLeader3("Gray Leader #3"),
+        GrayLeader4("Gray Leader #4"),
+        GrayLeader5("Gray Leader #5"),
+        GrayLeader6("Gray Leader #6");
+
+        private final String description;
+        private final Point coordinates;
+
+        /*
+    public EnemyFighters() {
+
+    }
+         */
+        EnemyFighters(String description) {
+            this.description = description;
+            coordinates = new Point(12, 8); // Change to new Point(12,8);
         }
-        return true;
+
+        public String getDescription() {
+            return description;
+        }
+
+        // Change to Point
+        public Point getCoordinates() {
+            return coordinates;
+        }
     }
 }
